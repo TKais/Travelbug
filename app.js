efc1642beead070880489465e923fc55b757d236
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2017-02-27 23:18:47 -0800

    Making progress on the first API call for Spotify

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
+var request = require('request');
+
 var Travelbug = (function() {
+
 	var getPlaylists = function(place) {
-		console.log(place);
+		_makeRequest(place);
+		// 'https://api.spotify.com/v1/search?q=' + place + '&type=playlist" -H "Accept: application/json'
 	};
 
 	var getShows = function(place, key) {
-		console.log(key);
+		// console.log(key);
 	};
+
+	var _makeRequest = function(place) {
+		var responseBody;
+		request('https://api.spotify.com/v1/search?q=' + place + '&type=playlist', function(error, response, body) {
+			if(!error && response.statusCode === 200) {
+				responseBody = JSON.parse(body);
+				console.log(responseBody);
+			}
+		});
+		return new Promise( function(resolve, reject) {
+			if(responseBody.playlists.items.length > 0) {
+				resolve(responseBody.playlists.items);
+			} else {
+				reject(Error('Cannot find playlists for ' + place));
+			}
+		});
+	};
+
+	// var showAll = function(items) {
+	// 	items.forEach(function(item) {
+	// 		console.log(item);
+	// 	});
+	// };
 
 	return {
 		getPlaylists : getPlaylists,
