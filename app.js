f5ddc1a5e94f7c7dd8059eb9abf9f68a2c7a065c
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2017-02-27 22:05:05 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var Travelbug = (function() {
+	var getPlaylists = function(place) {
+		console.log(place);
+	};
+
+	var getShows = function(place, key) {
+		console.log(key);
+	};
+
+	return {
+		getPlaylists : getPlaylists,
+		getShows : getShows
+	}
+
+})();
