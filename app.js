ac93a3f4e056153be9e65a0c116a03764a5fe5d3
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2017-03-07 19:38:07 -0800

    Return a promise in the request function to prevent response from returning before data is retrieved from the server

--- app.js
+++ app.js
@@ -2,37 +2,31 @@ var request = require('request');
 
 var Travelbug = (function() {
 
+	var _makeRequest = function(requestUrl) {
+		return new Promise( function(resolve, reject) {
+			request(requestUrl, function(error, response, body) {
+				if(!error && response.statusCode === 200) {
+					resolve(JSON.parse(body));
+				} else {
+					reject('Could not find any results for ' + place);
+				}
+			});
+		});
+	};
+	
+	var _sendResponse = function(response) {
+		console.log('RES--->', response);
+		return response;
+	}
+
 	var getPlaylists = function(place) {
-		_makeRequest(place);
-		// 'https://api.spotify.com/v1/search?q=' + place + '&type=playlist" -H "Accept: application/json'
+		_makeRequest('https://api.spotify.com/v1/search?q=' + place + '&type=playlist').then(_sendResponse);
 	};
 
 	var getShows = function(place, key) {
 		// console.log(key);
 	};
 
-	var _makeRequest = function(place) {
-		var responseBody;
-		request('https://api.spotify.com/v1/search?q=' + place + '&type=playlist', function(error, response, body) {
-			if(!error && response.statusCode === 200) {
-				responseBody = JSON.parse(body);
-				console.log(responseBody);
-			}
-		});
-		return new Promise( function(resolve, reject) {
-			if(responseBody.playlists.items.length > 0) {
-				resolve(responseBody.playlists.items);
-			} else {
-				reject(Error('Cannot find playlists for ' + place));
-			}
-		});
-	};
-
-	// var showAll = function(items) {
-	// 	items.forEach(function(item) {
-	// 		console.log(item);
-	// 	});
-	// };
 
 	return {
 		getPlaylists : getPlaylists,
