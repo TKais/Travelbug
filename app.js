6f035703b0e152d0af469c4db25bb56fde879837
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2017-05-14 19:55:33 -0700

    Return only relevant results

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ var request = require('request');
 
 const Travelbug = ( () => {
 
-	const _buildRequestUrl = (place, apiKey, music) => {
-	    const requestUrl = music ? 'https://api.spotify.com/v1/search?q=' + place + '&type=playlist' : 'http://api-public.guidebox.com/v2/shows\?api_key\=' + apiKey + '\&tags\=' + place;
+	const _buildRequestUrl = (place, apiKey, isSpotify) => {
+	    const requestUrl = isSpotify ? 'https://api.spotify.com/v1/search?q=' + place + '&type=playlist' : 'http://api-public.guidebox.com/v2/shows\?api_key\=' + apiKey + '\&tags\=' + place;
 	    return requestUrl;
 	}
 
-	const _makeRequest = (requestUrl) => {
+	const _makeRequest = (requestUrl, isSpotify) => {
 		return new Promise( (resolve, reject) => {
 			request(requestUrl, (error, response, body) => {
 				if(!error && response.statusCode === 200) {
-					resolve(JSON.parse(body));
+					isSpotify ? resolve(JSON.parse(body).playlists.items) : resolve(JSON.parse(body).results);
 				} else {
 					reject('Could not find any results for ' + place);
 				}
@@ -21,14 +21,14 @@ const Travelbug = ( () => {
 
 	const getPlaylists = (place) => {
 		const requestUrl = _buildRequestUrl(place, null, true);
-		const musicData = _makeRequest(requestUrl);
+		const musicData = _makeRequest(requestUrl, true);
 		return musicData;
 	};
 
 
 	const getShows = (place, apiKey) => {
 		const requestUrl = _buildRequestUrl(place, apiKey, false);
-		const showData = _makeRequest(requestUrl);
+		const showData = _makeRequest(requestUrl, false);
 		return showData;
 	};
 
