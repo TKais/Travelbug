d07624d15616326d31f6807eaa640f873a999bf0
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2021-04-15 20:50:56 -0700

    Fix spacing and use function declarations

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var request = require('request');
 
 const Travelbug = ( () => {
 
-  const _buildRequestUrl = (place, apiKey, isSpotify) => {
+  function _buildRequestUrl(place, apiKey, isSpotify) {
     const requestUrl = isSpotify ? 'https://api.spotify.com/v1/search?q=' + place + '&type=playlist' : 'http://api-public.guidebox.com/v2/shows\?api_key\=' + apiKey + '\&tags\=' + place;
     return requestUrl;
   }
 
-  const _makeRequest = (requestUrl, isSpotify) => {
+  function _makeRequest(requestUrl, isSpotify) {
     return new Promise( (resolve, reject) => {
       request(requestUrl, (error, response, body) => {
         if(!error && response.statusCode === 200) {
@@ -19,18 +19,18 @@ const Travelbug = ( () => {
     });
   }
 
-  const getPlaylists = (place) => {
+  function getPlaylists(place) {
     const requestUrl = _buildRequestUrl(place, null, true);
     const musicData = _makeRequest(requestUrl, true);
     return musicData;
   };
 
 
-  const getShows = (place, apiKey) => {
+  function getShows(place, apiKey) {
     const requestUrl = _buildRequestUrl(place, apiKey, false);
     const showData = _makeRequest(requestUrl, false);
     return showData;
-};
+  };
 
 
   return {
